a67de712d1f51b65c1eec51e04758cfee527f968
ashish-greycube <mr.ashish.shah@gmail.com>
2022-02-15 12:14:20 +0530

    fix: qty validation, multiple msg and remove zero qty

--- almofa/almofa/doctype/tender_info/tender_info.js
+++ almofa/almofa/doctype/tender_info/tender_info.js
@@ -3,10 +3,22 @@
 
 frappe.ui.form.on('Tender Info', {
 	refresh: function(frm) {
-		if (frm.doc.docstatus != 2 && frm.doc.tender_info_item_detail && frm.doc.tender_info_item_detail.length>0) {
+		if (frm.doc.docstatus == 1 && frm.doc.tender_info_item_detail && frm.doc.tender_info_item_detail.length>0) {
 			frm.add_custom_button(__('Quotation'), () => create_quotation_from_tender_info(), __('Create'));
 			frm.add_custom_button(__('Sales Order'), () => create_sales_order_from_tender_info(), __('Create'));
 		}
+	}
+});
+
+frappe.ui.form.on('Tender Info Item Detail', {
+	qty: function(frm,cdt,cdn) {
+		let row = locals[cdt][cdn]
+		if (row.quoted_qty && row.qty< row.quoted_qty) {
+			frappe.throw(__('Qty entered cannot be less than quoted qty'))
+		}
+		if (row.ordered_qty && row.qty< row.ordered_qty) {
+			frappe.throw(__('Qty entered cannot be less than ordered qty'))
+		}		
 	}
 });
 
--- almofa/almofa/doctype/tender_info/tender_info.py
+++ almofa/almofa/doctype/tender_info/tender_info.py
@@ -12,13 +12,14 @@ class TenderInfo(Document):
 	def validate(self):
 		self.validate_for_duplicate_items()			
 
+	def on_update_after_submit(self):
 		for ti_item in self.tender_info_item_detail:
-			if ti_item.quoted_qty>ti_item.qty:
-				msg = _('Row #{0} : {1} item, "quoted qty" cannot be greater than "qty" for tender info {2}.'.format(ti_item.idx,ti_item.item,frappe.bold(get_link_to_form('Tender Info',self.name))))
+			if ti_item.quoted_qty and ti_item.quoted_qty>ti_item.qty:
+				msg = _('Row #{0} : {1} item, "quoted qty" cannot be greater than <b>{2}</b> "qty" for tender info {3}.'.format(ti_item.idx,ti_item.item,ti_item.quoted_qty-ti_item.qty,frappe.bold(get_link_to_form('Tender Info',self.name))))
 				frappe.throw(msg)					
 
-			if ti_item.ordered_qty>ti_item.qty:
-				msg = _('Row #{0} : {1} item, "ordered qty" cannot be greater than "qty" for tender info {2}.'.format(ti_item.idx,ti_item.item,frappe.bold(get_link_to_form('Tender Info',self.name))))
+			if ti_item.ordered_qty and ti_item.ordered_qty>ti_item.qty:
+				msg = _('Row #{0} : {1} item, "ordered qty" cannot be greater than <b>{2}</b> "qty" for tender info {3}.'.format(ti_item.idx,ti_item.item,ti_item.ordered_qty-ti_item.qty,frappe.bold(get_link_to_form('Tender Info',self.name))))
 				frappe.throw(msg)	
 
 	def validate_for_duplicate_items(self):
@@ -34,13 +35,12 @@ class TenderInfo(Document):
 def create_sales_order_from_tender_info(source_name, target_doc=None):
 	def set_missing_values(source, target):
 		target.customer=source.customer
-		# source.tender_date
-		# delivery_date
 
 	def update_item(source_doc, target_doc, source_parent):
+		target_qty=(source_doc.qty-source_doc.ordered_qty)
 		target_doc.item_code = source_doc.item
 		target_doc.item_name = source_doc.item_name
-		target_doc.qty = (source_doc.qty-source_doc.ordered_qty)
+		target_doc.qty = target_qty
 
 	ti=frappe.get_doc('Tender Info',source_name)
 	eligible_item=False
@@ -65,12 +65,12 @@ def create_sales_order_from_tender_info(source_name, target_doc=None):
 		},
 		"Tender Info Item Detail": {
 			"doctype": "Sales Order Item",
-			"postprocess": update_item
+			"postprocess": update_item,
+			"condition": lambda doc: (doc.qty-doc.ordered_qty)>0
 		},		
 	}, target_doc,set_missing_values)
 	doc.run_method('set_missing_values')
 	return doc
-
 
 
 @frappe.whitelist()
@@ -80,9 +80,10 @@ def create_quotation_from_tender_info(source_name, target_doc=None):
 		target.party_name=source.customer
 
 	def update_item(source_doc, target_doc, source_parent):
+		target_qty=(source_doc.qty-source_doc.quoted_qty)
 		target_doc.item_code = source_doc.item
 		target_doc.item_name = source_doc.item_name
-		target_doc.qty = (source_doc.qty-source_doc.quoted_qty)
+		target_doc.qty = target_qty
 
 	ti=frappe.get_doc('Tender Info',source_name)
 	eligible_item=False
@@ -107,43 +108,41 @@ def create_quotation_from_tender_info(source_name, target_doc=None):
 		},
 		"Tender Info Item Detail": {
 			"doctype": "Quotation Item",
-			"postprocess": update_item
+			"postprocess": update_item,
+			"condition": lambda doc: (doc.qty-doc.quoted_qty)>0
 		},		
 	}, target_doc,set_missing_values)
 	doc.run_method('set_missing_values')
 	return doc
 
 
-
-
-
 def update_quoted_qty_of_tender_info(self,method):
 	if self.tender_info_cf:
 		ti=frappe.get_doc('Tender Info',self.tender_info_cf)
 		qo_item_found=False
-
+		msg=''
 		for item in self.items:
 			for ti_item in ti.tender_info_item_detail:
 				if item.item_code == ti_item.item:
 					if self.doctype=='Quotation' and method=='on_submit':
 						ti_item.quoted_qty=ti_item.quoted_qty+item.qty
-						msg = _('Row #{0} : {1} item quoted qty is updated for tender info {2}.'.format(ti_item.idx,ti_item.item,frappe.bold(get_link_to_form('Tender Info',self.tender_info_cf))))
+						msg += _('Row #{0} : {1} item quoted qty is updated for tender info {2}. <br>'.format(ti_item.idx,ti_item.item,frappe.bold(get_link_to_form('Tender Info',self.tender_info_cf))))
 						qo_item_found=True
 					elif self.doctype=='Quotation' and method=='on_cancel':
 						ti_item.quoted_qty=ti_item.quoted_qty-item.qty
-						msg = _('Row #{0} : {1} item quoted qty is updated for tender info {2}.'.format(ti_item.idx,ti_item.item,frappe.bold(get_link_to_form('Tender Info',self.tender_info_cf))))
+						msg += _('Row #{0} : {1} item quoted qty is updated for tender info {2}. <br>'.format(ti_item.idx,ti_item.item,frappe.bold(get_link_to_form('Tender Info',self.tender_info_cf))))
 						qo_item_found=True
 					elif self.doctype=='Sales Order' and (method=='on_submit' or method=='on_update_after_submit'):
 						ti_item.ordered_qty=ti_item.ordered_qty+item.qty
-						msg = _('Row #{0} : {1} item ordered qty is updated for tender info {2}.'.format(ti_item.idx,ti_item.item,frappe.bold(get_link_to_form('Tender Info',self.tender_info_cf))))
+						msg += _('Row #{0} : {1} item ordered qty is updated for tender info {2}. <br>'.format(ti_item.idx,ti_item.item,frappe.bold(get_link_to_form('Tender Info',self.tender_info_cf))))
 						qo_item_found=True
 					elif self.doctype=='Sales Order' and method=='on_cancel':
 						ti_item.ordered_qty=ti_item.ordered_qty-item.qty
-						msg = _('Row #{0} : {1} item ordered qty is updated for tender info {2}.'.format(ti_item.idx,ti_item.item,frappe.bold(get_link_to_form('Tender Info',self.tender_info_cf))))
+						msg += _('Row #{0} : {1} item ordered qty is updated for tender info {2}. <br>'.format(ti_item.idx,ti_item.item,frappe.bold(get_link_to_form('Tender Info',self.tender_info_cf))))
 						qo_item_found=True						
 		if qo_item_found==True:
 			ti.save(ignore_permissions=True)
-			if msg:
+			if len(msg)>0:
 				frappe.msgprint(msg)
 
 def validate_against_tender_info(self,method):
