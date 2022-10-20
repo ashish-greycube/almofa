983ad4e6999645da6d934fd2c4ea28415046516d
ashish-greycube <mr.ashish.shah@gmail.com>
2022-10-20 14:59:09 +0530

    fix: net profit value

--- almofa/almofa/report/almofa_net_profit/almofa_net_profit.py
+++ almofa/almofa/report/almofa_net_profit/almofa_net_profit.py
@@ -5,32 +5,45 @@ from __future__ import unicode_literals
 
 import frappe
 
-
 def execute(filters=None):
     return get_columns(filters), get_data(filters)
 
+def get_columns(filters):
+    return csv_to_columns(
+        """
+        Sales Invoice,name,Link,Sales Invoice,140
+        Customer,customer,Link,Customer,260
+        Customer Group,customer_group,Link,Customer Group,200
+        Posting Date,posting_date,Date,,120
+        Selling Amount,base_net_total,Currency,,120,1
+        Buying Amount,buying_amount,Currency,,120,1
+        Gross Profit,gross_profit,Currency,,120,1
+        Profit Percent,gross_profit_pct,Percent,,120,1
+        Collection Fees,collection_fees,Currency,,120,1
+        Transport Fees,transport_fees,Currency,,120,1
+        Delay Fine,delay_fine,Currency,,120,1
+        Total Expense,total_expense,Currency,,120,1
+        Net Profit,net_profit,Currency,,120,1
+        Net Profit %,net_profit_pct,Percent,,120,1
+    """
+    )
 
 def get_data(filters):
+# (coalesce(tpi_oex.amount,0) + coalesce(oex.amount,0)) as other_expenses ,
     return frappe.db.sql(
         """
 select 
 tsi.name , tsi.customer , tsi.customer_group , tsi.posting_date ,
-tsi.base_net_total , coalesce(si_cogs.debit, 0) + coalesce(cogs.debit, 0) buying_amount , 
-tsi.base_net_total - coalesce(si_cogs.debit, 0) - coalesce(cogs.debit,0) gross_profit, 
-round((tsi.base_net_total - coalesce(si_cogs.debit, 0) - coalesce(cogs.debit,0))/tsi.base_net_total * 100, 2) gross_profit_pct ,
-coalesce(tpi_coll.amount,0) + coalesce(coll.amount,0) collection_fees , 
-coalesce(tpi_trans.amount,0) + coalesce(trans.amount,0) transport_fees , 
-coalesce(tpi_dlay.amount,0) + coalesce(dlay.amount,0) dlay_fine , 
-coalesce(tpi_oex.amount,0) + coalesce(oex.amount,0) other_expenses ,
-coalesce(tpi_coll.amount,0) + coalesce(coll.amount,0) + 
-coalesce(tpi_dlay.amount,0) + coalesce(dlay.amount,0) + 
-coalesce(tpi_trans.amount,0) + coalesce(trans.amount,0) total_expense ,
-tsi.base_net_total - coalesce(si_cogs.debit, 0)- coalesce(cogs.debit,0) 
-- coalesce(dlay.amount,0) - coalesce(trans.amount,0) - coalesce(coll.amount,0) 
-- coalesce(tpi_dlay.amount,0) - coalesce(tpi_trans.amount,0) - coalesce(tpi_coll.amount,0) net_profit , 
-round(100 * (tsi.base_net_total - coalesce(si_cogs.debit, 0) - coalesce(cogs.debit,0) 
-- coalesce(dlay.amount,0) - coalesce(trans.amount,0) - coalesce(coll.amount,0)
-- coalesce(tpi_dlay.amount,0) - coalesce(tpi_trans.amount,0) - coalesce(tpi_coll.amount,0)) /tsi.base_net_total,2) net_profit_pct
+tsi.base_net_total , 
+(coalesce(si_cogs.debit, 0) + coalesce(cogs.debit, 0)) as buying_amount , 
+(tsi.base_net_total - coalesce(si_cogs.debit, 0) - coalesce(cogs.debit,0) ) as gross_profit, 
+round((tsi.base_net_total - coalesce(si_cogs.debit, 0) - coalesce(cogs.debit,0))/tsi.base_net_total * 100, 2) as gross_profit_pct ,
+(coalesce(tpi_coll.amount,0) + coalesce(coll.amount,0)) as collection_fees , 
+(coalesce(tpi_trans.amount,0) + coalesce(trans.amount,0)) as transport_fees , 
+( coalesce(tpi_dlay.amount,0) + coalesce(dlay.amount,0)) as delay_fine , 
+(coalesce(tpi_coll.amount,0) + coalesce(coll.amount,0) + coalesce(tpi_dlay.amount,0) + coalesce(dlay.amount,0) + coalesce(tpi_trans.amount,0) + coalesce(trans.amount,0) ) as total_expense ,
+((SELECT gross_profit)-(SELECT total_expense)) as net_profit , 
+((SELECT net_profit)/(tsi.base_net_total))*100 net_profit_pct
 from `tabSales Invoice` tsi
 left outer join (
     select voucher_no , sum(debit) debit from `tabGL Entry` tge 
@@ -135,29 +148,11 @@ order by tsi.name
     )
 
 
-def get_columns(filters):
-    return csv_to_columns(
-        """
-        Sales Invoice,name,Link,Sales Invoice,140
-        Customer,customer,Link,Customer,260
-        Customer Group,customer_group,Link,Customer Group,200
-        Posting Date,posting_date,Date,120
-        Selling Amount,base_net_total,Currency,120
-        Buying Amount,buying_amount,Currency,120
-        Gross Profit,gross_profit,Currency,120
-        Profit Percent,profit_percent,Percent,120
-        Collection Fees,collection_fees,Currency,120
-        Transport Fees,transport_fees,Currency,120
-        Delay Fine,delay_fine,Currency,120
-        Total Expense,total_expense,Currency,120
-        Net Profit,net_profit,Currency,120
-        Net Profit %,net_profit_percent,Percent,120
-    """
-    )
+
 
 
 def csv_to_columns(csv_str):
-    props = ["label", "fieldname", "fieldtype", "options", "width"]
+    props = ["label", "fieldname", "fieldtype", "options", "width","precision"]
     return [
         zip(props, [x.strip() for x in col.split(",")])
         for col in csv_str.split("\n")
